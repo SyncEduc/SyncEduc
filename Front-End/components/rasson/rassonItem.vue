c6364aae78f824760aeebd978bfcf11b6811fd56
Luis Davi <luisdavi.contato@gmail.com>
2023-08-17 13:16:54 -0300

    added sections in home page

---
+++ Front-End/components/rasson/rassonItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="RassonItemContainer">
+    <div class="imageContainer">
+      <slot name="image"/>
+    </div>
+    <slot name="title"/>
+    <div class="descContainer">
+      <div class="square"></div>
+      <slot name="desc"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.RassonItemContainer{
+  @apply w-52 h-52 hover:h-max
+  flex flex-col items-center justify-center gap-2
+  rounded-[25px]
+  border border-black
+  transition-all
+  text-center
+  relative
+  p-2
+}
+.RassonItemContainer > div.imageContainer{
+  @apply w-32 h-32
+}
+.RassonItemContainer > #title {
+  @apply w-full text-center
+}
+.RassonItemContainer > div.descContainer{
+  box-shadow: 0px 20px 20px -5px rgba(0, 0, 0, 0.40);
+  @apply w-52 text-sm hidden 
+  top-[110%] z-50
+  absolute bg-white
+  text-black/50
+  rounded-[25px]
+  items-center justify-center
+  text-center p-2 transition-all border border-black
+}
+.RassonItemContainer > div.descContainer > div.square{
+  @apply w-[16px] h-[16px] -top-[8px] rotate-45
+   absolute border-t border-l border-black bg-white
+}
+.RassonItemContainer:hover > div.descContainer{
+  @apply flex text-center 
+}
+</style>
--- Front-End/pages/index.vue
+++ Front-End/pages/index.vue
@@ -1,11 +1,97 @@
 <template>
-    <div class="">
-        <a href="/login" class="bg-blue-500 rounded-lg hover:scale-110 p-5">Login</a>
-    </div>
+    <NuxtLayout name="nav">
+        <div class="HomeContainer">
+            <section class="SectionWelcome">
+
+            </section>
+            <section class="SectionRasson">
+                <h1>
+                    Por que escolher nossos cursos?
+                </h1>
+                <div>
+                    <RassonItem v-for="(item, index) in rassons" :key="index">
+                        <template #image>
+                            <img id="img" :src="item.img">
+                        </template>
+                        <template #title>
+                            <h1 id="title"> {{ item.title }}</h1>
+                        </template>
+                        <template #desc>
+                            <p class="RassonDesc" id="desc">{{ item.desc }}</p>
+                        </template>
+                    </RassonItem>
+                </div>
+            </section>
+
+        </div>
+    </NuxtLayout>
 </template>
+<script setup>
+import know from '../assets/images/know.png'
+import clock from '../assets/images/clock.png'
+import diversity from '../assets/images/diversidade.png'
+import commnity from '../assets/images/community.png'
+import certificate from '../assets/images/certificate.png'
+const rassons = ref([
+    {
+        img: know,
+        title: 'Aprendizado Interativo',
+        desc: 'Acreditamos que aprender a programar deve ser envolvente e interativo. Nossos cursos incluem desafios práticos, projetos do mundo real e quizzes para garantir que você realmente compreenda os conceitos e possa aplicá-los com confiança.'
+    },
+    {
+        img: clock,
+        title: 'Flexibilidade de horário',
+        desc: 'A vida é agitada, e entendemos que o aprendizado precisa se adaptar à sua rotina. Nossos cursos são autoinstrucionais, permitindo que você avance no seu próprio ritmo e reveja as aulas sempre que precisar.'
+    },
+    {
+        img: diversity,
+        title: 'Diversidade de cursos',
+        desc: 'Nossa extensa gama de cursos abrange desde as linguagens de programação mais populares até as tecnologias emergentes do momento. Com a orientação de instrutores apaixonados e especializados, você poderá dominar Python, Java, JavaScript, C++ e muito mais.'
+    },
+    {
+        img: commnity,
+        title: 'Comunidade Ativa',
+        desc: 'Junte-se a uma comunidade vibrante de alunos, entusiastas e profissionais da área. Compartilhe dúvidas, troque ideias e colabore em projetos conjuntos, criando conexões valiosas que podem impulsionar sua carreira.'
+    },
+    {
+        img: certificate,
+        title: 'Diversidade de cursos',
+        desc: 'Ao concluir um curso, você receberá um certificado de conclusão que pode enriquecer seu currículo e destacar suas habilidades aos olhos dos empregadores.'
+    }
+])
+</script>
 <style>
-*{
-    @apply transition-all
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap');
+
+* {
+    font-family: 'JetBrains Mono', monospace;
+}
+</style>
+<style scoped>
+.HomeContainer {
+    @apply w-full min-h-screen flex flex-col
+}
+
+.HomeContainer>section {
+    @apply w-full min-h-screen py-4 px-6
+}
+
+.HomeContainer>section.SectionWelcome {
+    background-image: url('../assets/images/homeBackground.png');
+    background-size: cover;
+}
+
+.HomeContainer>section.SectionRasson {
+    @apply bg-white -mt-16 h-[150vh] rounded-t-[50px] flex flex-col
+     gap-12 items-center justify-start
+}
+
+.HomeContainer>section.SectionRasson>h1 {
+    @apply text-4xl
+}
+
+.HomeContainer>section.SectionRasson>div {
+    @apply w-[70%] flex flex-row gap-24 flex-wrap justify-center
 }
 </style>
   
